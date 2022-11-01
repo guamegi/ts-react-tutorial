c57dbc1abe14000f24af0daf1fdde76705c89238
Minha Kwon <namisoft@naver.com>
2022-11-01 16:22:56 +0900

    useReducer 사용 state 관리

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import Greetings from "./Greetings";
+// import Counter from "./Counter";
+// import MyForm from "./MyForm";
+import ReducerSample from "./ReducerSample";
 
 function App() {
-  const onClick = (name: string) => {
-    console.log(`${name} says hello`);
-  };
-  return <Greetings name="Hello" optional="aa" onClick={onClick} />;
+  // const onSubmit = (form: { name: string; description: string }) => {
+  //   console.log(form);
+  // };
+  // return <MyForm onSubmit={onSubmit} />;
+  // return <Counter />;
+  return <ReducerSample />;
 }
 
 export default App;
